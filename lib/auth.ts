2d194a48e2aaa86732c13f8030fc511d356bf4f5
akarsu35 <cumhurakarsu@gmail.com>
2026-02-02 22:44:13 +0300

    better auth2

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,21 +2,26 @@ import { betterAuth } from 'better-auth'
 import { prismaAdapter } from 'better-auth/adapters/prisma'
 import prisma from '@/services/db'
 
-const baseURL =
-  process.env.BETTER_AUTH_URL ??
-  process.env.NEXT_PUBLIC_APP_URL ??
-  'http://localhost:3000'
-
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: 'postgresql',
   }),
-  baseURL,
-  secret: process.env.BETTER_AUTH_SECRET,
-  trustedOrigins: [baseURL],
+
+  // ⬇️ MUTLAKA env’den gelsin
+  baseURL: process.env.BETTER_AUTH_URL!,
+
+  secret: process.env.BETTER_AUTH_SECRET!,
+
+  // ⬇️ Vercel domain’i trusted origin yapıyoruz
+  trustedOrigins: [
+    process.env.BETTER_AUTH_URL!,
+    process.env.NEXT_PUBLIC_APP_URL!,
+  ],
+
   emailAndPassword: {
     enabled: true,
   },
+
   logger: {
     disabled: false,
     level: 'debug',
